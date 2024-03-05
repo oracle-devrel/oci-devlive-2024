2727212539be5a039bfa379cbefd94a096bd7e02
Eli Schilling <schilling.eli@gmail.com>
2024-03-05 08:10:06 -0800

    Minor adjustments to automation

---
+++ sb-hol/enableapm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Apply Cert-Manager
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.3/cert-manager.yaml
+
+# Apply K8 OpenTelemetry Operator
+kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
+
+# Configure APM agent using K8 operator
+kubectl apply -f ~/oci-devlive-2024/sb-hol/customapmresource.yaml
+
+# Appyly APM java agent through the K8 namespace
+kubectl apply -f ~/oci-devlive-2024/sb-hol/apmnamespace.yaml
+
+# Redeploy application
+kubectl apply -f ~/oci-devlive-2024/sb-hol/wstore.yaml
